249989b6eac12ef5f9cb07cd78cbc5a351fa83b7
chenhuiyi <chenhuiyi@qinsilk.com>
2022-12-01 15:22:35 +0800

    12.1下午补充毕设内容

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import pinia from './pinia'
 
+import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+import '@/styles/index.scss'
+
+// 全局变量
+import './assets/icon/iconfont.css'
+
 createApp(App).use(pinia).use(router).mount('#app')
